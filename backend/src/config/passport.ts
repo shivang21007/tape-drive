cdd76f4509c6cc8a1a4e729521051feed9d28251
shivang21007 <shivanggupta2611@gmail.com>
2025-06-12 19:03:47 +0530

    very minor fix

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -76,7 +76,7 @@ const setupPassport = async () => {
         {
           clientID: process.env.GOOGLE_CLIENT_ID!,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-          callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:8000/auth/google/callback',
+          callbackURL: process.env.GOOGLE_CALLBACK_URL || process.env.BACKEND_NODE_ENV === 'production' ? 'https://tapeutilsbk.octro.com/auth/google/callback' : 'http://localhost:8000/auth/google/callback'
         },
         async (accessToken, refreshToken, profile, done) => {
           try {
